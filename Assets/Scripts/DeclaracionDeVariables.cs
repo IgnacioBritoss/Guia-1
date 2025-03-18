bf5260796c946ceb77df0973fb5768dc177facdb
IgnacioBritoss <167232030+IgnacioBritoss@users.noreply.github.com>
2025-03-18 12:06:34 -0300

    variables declaradas

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int cantidadAlumnos;
+    float magnitudTerremotos;
+    string nombreAlumno;
+    bool computadoraHabilitada;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.("hola");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
